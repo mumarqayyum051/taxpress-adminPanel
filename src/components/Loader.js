65cf0a985d8fc926ef4a3f1c511bd4c73dc2eb50
mumarqayyum051 <mumarqayyum051>
2022-06-03 15:07:02 +0500

    loader added

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,32 @@
-import * as React from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import { useState } from 'react';
+import { css } from '@emotion/react';
+import ClipLoader from 'react-spinners/ClipLoader';
 
-export default function Loader() {
+// Can be a string as well. Need to ensure each key-value pair ends with ;
+const override = css`
+  margin: 0 auto;
+  border-color: #28a793;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+`;
+
+const loaderParent = {
+  position: 'relative',
+};
+
+function Loader() {
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState('#ffffff');
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
-    </Box>
+    <div style={loaderParent}>
+      <ClipLoader color={color} loading={loading} css={override} size={150} />
+    </div>
   );
 }
+
+export default Loader;
